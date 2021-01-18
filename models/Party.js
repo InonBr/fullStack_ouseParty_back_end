0673744c7e551770a9c6ede38309729295b32489
Inon Bercu <inonbr@gmail.com>
2021-01-18 19:30:27 +0200

    partyId must be unique

--- models/Party.js
+++ models/Party.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const PartySchema = new mongoose.Schema({
-    userId: {
-        required: true,
-        type: String
-    },
-    date: {
-        required: true,
-        type: String
+  userId: {
+    required: true,
+    type: String,
+  },
+  date: {
+    required: true,
+    type: String,
+  },
+  partyId: {
+    required: true,
+    unique: true,
+    type: String,
+  },
+});
 
-    },
-    partyId: {
-        required: true,
-        type: String
-    }
-})
-
-module.exports = Party = mongoose.model('parties', PartySchema)
+module.exports = Party = mongoose.model('parties', PartySchema);
