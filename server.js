e42b6d7d55e636fff38acbd94af682204d49ba5e
Inon Bercu <inonbr@gmail.com>
2021-01-24 16:00:27 +0200

    spotify log in

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const homeRouter = require('./routers/homeRouter');
 const userRouter = require('./routers/userRouter');
 const partyRouter = require('./routers/partyRouter');
+const spotifylogin = require('./routers/spotifyLogin');
 const connectDB = require('./db/db');
 
 const port = 5000;
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(homeRouter);
 app.use(userRouter);
 app.use(partyRouter);
+app.use(spotifylogin);
 
 app.listen(port, () => {
   console.log(`🟢 App listening at http://localhost:${port}`);
